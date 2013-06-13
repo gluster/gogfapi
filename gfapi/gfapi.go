b62b91e7c18ed2670bfae78901660fff8510a6f6
Kaushal M <kshlmster@gmail.com>
2013-06-13 10:32:21 +0530

    Move package to subdirectory and rename back to gfapi. Makes it easier to use after importing.

--- gfapi/gfapi.go
+++ gfapi/gfapi.go
@@ -1,6 +1,6 @@
 // Package gfapi provides a wrapper around gfapi, the GlusterFS api, which is used to access files/directories on a Gluster volume.
 // The design tries to follow the default go file handling functions provided by the os package as much as possible.
-package gogfapi
+package gfapi
 
 // #cgo pkg-config: glusterfs-api
 // #include "api/glfs.h"
